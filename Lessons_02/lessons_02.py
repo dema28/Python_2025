3e1df5fcca513e55e509426e3a546b810b15a1bc
Denis Novicov <dema28ster@gmail.com>
2025-02-16 09:20:29 +0100

    Lessons_02

---
+++ Lessons_02/lessons_02.py
@@ -0,0 +1,114 @@
+# # Задача 1
+# print("Hello, world!")
+#
+#
+# # Задача 2
+# print("Я люблю:")
+# print("1. Фильмы")
+# print("2. Гулять")
+# print("3. Читать")
+#
+# name = "Света"
+#
+# print("Привет", name)
+# print("Как дела", name)
+# print("Пока", name)
+
+
+# a = 'Строка' # str
+# b = 100 # int
+# c = 3.14 # float
+# d = False # bool
+# e = None
+#
+# print(a)
+
+# name = None
+# name = "Mike"
+#
+# print(name)
+
+# a = 10
+# b = 22
+#
+# print(a + b)
+# print(a - b)
+# print(a / b)
+# print(10 // 3)
+# print(a * b)
+# print(a % b)
+# print(a ** b)
+
+# a = 5
+# b = 12
+# result = (a + b) * 5
+#
+# print(result)
+
+
+# a = 10
+# b = "51" # -> 5
+# int_b = int(b)
+#
+# print(a + int_b)
+
+# a = 10 # int
+# b = str(a) # str
+#
+# print("Привет" + b)
+
+#
+# name = input("Введите свое имя:")
+#
+# print("Привет,", name)
+
+# a = input("Введите первое число: ")
+# b = input("Введите второе число: ")
+# int_a = int(a)
+# int_b = int(b)
+#
+# print(int_a + int_b)
+
+
+# name = "Алексей"
+# surname = "Морозов"
+# fullname = name + " " + surname
+#
+# print(fullname)
+
+
+# name = "Алексей"
+# surname = "Морозов"
+# age = 25
+# text = f"Привет, меня зовут {name} {surname}! Мне {age} лет."
+#
+# print(text)
+
+# text = "привет, Меня зовут Маша"
+# upper_text = text.upper()
+#
+# print(text)
+# print(upper_text)
+# print(text.lower())
+
+# name = input("Введите имя: ")
+# surname = input("Введите фамилию: ")
+# fullname = f"{name} {surname}"
+# print(f"Полное имя, которое ввел пользователь: {fullname}")
+# title_fullname = fullname.title()
+#
+# print(title_fullname)
+
+# name = input("Введите имя: ").upper()
+#
+# print(name)
+
+# print("*" * 20)
+# print("ДОБРО ПОЖАЛОВАТЬ!")
+# print("*" * 20)
+
+
+
+
+
+
