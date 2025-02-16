8880edbfc4fd0e52149dec190b8f34ddf5359031
Denis Novicov <dema28ster@gmail.com>
2025-02-16 09:20:46 +0100

    Lessons_03

---
+++ Lessons_03/lessons_03.py
@@ -0,0 +1,203 @@
+# a = True
+# b = False
+#
+# print(a, b)
+
+
+# a = 1 > 4 # True
+#
+# print(a)
+# print(1 > 4)
+# print(1 < 4)
+# print(1 >= 4)
+# print(1 <= 4)
+# print(4 == 4) # True
+# print(1 != 4)
+
+# a = "abc" == "abc" # True
+# b = "aBc" == "abc" # False
+# c = "cvd" != "aaa"
+# d = "Hwq" < "G"
+#
+# print(a, b, c, d)
+
+
+# age = 16
+#
+# if age >= 18:
+#     print("Добро пожаловать!")
+#     print("Привет")
+# else:
+#     print("Доступ запрещен :(")
+#
+# print("Продолжение программы...")
+
+# age = 15
+# is_admin = True
+#
+# if age >= 18:
+#     print("Добро пожаловать!")
+#     print("Привет")
+# elif is_admin == True:
+#     print("Привет, админ!")
+# else:
+#     print("Доступ запрещен :(")
+#
+# print(is_admin)
+# print("Продолжение программы...")
+
+# age = 21
+# is_citizen = False
+# is_admin = True
+#
+# if age >= 18 and is_citizen == True:
+#     print("Вы можете голосвать!")
+# else:
+#     print("Вы не можете голосовать!")
+#
+# print("Продлолжение...")
+#
+#
+# is_student = False
+# is_senior = True
+#
+# if is_student or is_senior:
+#     print("Вам положена скидка!")
+# else:
+#     print("Скидки не будет")
+#
+# age = 12
+# is_admin = True
+#
+# if age >= 18 and is_admin:
+#     print("...")
+
+
+# age = int(input()) # 12
+#
+# if age >= 18:
+#     print("Вы взрослый!")
+# else:
+#     print("Вы не взрослый")
+
+# 1, -1, "abc", True, 3.14, ...
+# 0, None, False, "", ...
+
+# count = 0
+#
+# if count:
+#     print(f"У меня есть деньги! {count}$")
+# else:
+#     print("У меня нет денег :(")
+
+# string1 = "Hello"
+# string2 = "hello"
+#
+# print(string1.lower() == string2.lower())
+
+# print("abca" in "123123abc")
+
+# string = "Hello       "
+#
+# print(len(string) < 10)
+
+# range(5) # (0, 1, 2, 3, 4)
+
+# for i in range(5): # (0, 1, 2, 3, 4)
+#     print(i)
+
+# for c in "Hello":
+#     print(c)
+#     print("---")
+
+# for i in range(12):
+#     print(i)
+
+# string = "Hello"
+# n = 5
+#
+# for _ in range(n):
+#     print(string)
+
+# i, j, k
+# for i in range(10):
+#     print(i)
+#
+# # char
+# for c in "Hello":
+#     print(c)
+
+# string = "Hello world!"
+# n = 0
+#
+# for c in string:
+#     print(f"Проверяю букву {c}")
+#     if c == "l":
+#         print("Есть!")
+#         n += 1
+#     else:
+#         print("Это не буква l")
+#
+# print(n)
+#
+# n = 0
+#
+# for i in range(5): # 0, 1, 2, 3, 4
+#     n += i
+#
+# print(n)
+
+# password = "klsdkdfnk*fd"
+#
+# for c in password:
+#     if c == "*":
+#         print("* - под запретом!")
+#         break
+#     print(c)
+#
+# print("Продолжение программы...")
+
+# password = "klsdkdfnk*fd"
+#
+# for c in password:
+#     if c == "*":
+#         continue
+#     print(c)
+
+# password = "*ls*dkdfnkfddgdfgfg3434"
+# is_correct = True
+#
+# for c in password:
+#     if c == "*":
+#         is_correct = False
+#         break
+#     print(c)
+
+# if is_correct:
+#     print("Пароль хороший!")
+# else:
+#     print("Недопустимый пароль!")
+#
+# print("Продолжение программы...")
+
+# string = ""
+#
+# while string != "exit":
+#     string = input()
+#     print(string, "!!!!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
