8205d29c58f49d239fd108a40658e0bd7b2a46d4
Denis Novicov <dema28ster@gmail.com>
2025-03-03 03:11:17 +0100

    Lessons_05

---
+++ Lessons_05/lessons_05.py
@@ -0,0 +1,190 @@
+# print("hello!")
+# print()
+#
+# result = sorted([1, 2, 3])
+
+# def greet():
+#     print("*****")
+#     print("Hello!")
+#     print("*****")
+#     print()
+#
+#
+# def black():
+#     print("black")
+#
+#
+# greet()
+# greet()
+# greet()
+# black()
+
+# def greet(name):
+#     print("*****")
+#     print(f"Hello, {name}!")
+#     print("*****")
+#     print()
+#
+#
+# greet("Alex")
+# greet("Oksana")
+
+# def greet(name, char):
+#     print(char * 10)
+#     print(f"Hello, {name}!")
+#     print(char * 10)
+#     print()
+#
+#
+# greet("Oksana", "*") # greet("Oksana", "*")
+# greet(char="-", name="Mike")
+
+# print("Hello", "world", )
+
+# def greet(name, char="*"):
+#     print(char * 10)
+#     print(f"Hello, {name}!")
+#     print(char * 10)
+#     print()
+#
+#
+# greet("Oksana")
+# greet("Mike", "-")
+
+# print("Hello", "world", "!", sep="*", end="3\n")
+# print("Hello", "world", "!", sep="*", end="3\n")
+# print("Hello", "world", "!", sep="*", end="3\n")
+
+# def greet(name, char="*"):
+#     print(char * 10)
+#     print(f"Hello, {name}!")
+#     print(char * 10)
+#     print()
+
+
+# def my_sum(a, b, *args):
+#     total = 0
+#     for i in args:
+#         total += i
+#
+#     print(total)
+#
+#
+# def my_print(*args):
+#     for arg in args:
+#         print(arg)
+#
+# my_print(1, 2, 3, 4, 5, 6)
+
+
+# print("Hello")
+#
+# numbers = [1, 2, 3, 4, 5]
+# result = sum(numbers)
+#
+# print(result)
+
+# def my_sum(nums):
+#     total = 0
+#
+#     for num in nums:
+#         total += num
+#
+#     return total
+#
+#
+# numbers = [1, 2, 3, 4, 5]
+# result = my_sum("hello")
+#
+# print(result)
+
+# a = 5
+# b = 10
+#
+# def add(a, b):
+#     """
+#     Суммирует два числа.
+#
+#     :param a: Первое число.
+#     :param b: Второе число.
+#     :return: Сумма чисел.
+#     """
+#
+#     return a + b
+#
+#
+# def mul(a, b):
+#     """
+#     Перемножает два числа.
+#
+#     :param a: Первое число.
+#     :param b: Второе число.
+#     :return: Произведение чисел.
+#     """
+#
+#     return a * b
+#
+#
+# result_1 = add(1, 2)
+# result_2 = mul(5, 5)
+#
+# print(result_1, result_2)
+#
+# a = 10
+#
+# def func():
+#     global a
+#
+#     a += 2
+#
+# func()
+# print(a)
+
+# n = 5
+# n = "Hello"
+# print(n)
+#
+# def add(a: int, b: int) -> int:
+#     """
+#
+#     :param a:
+#     :param b:
+#     :return:
+#     """
+#
+#     return a + b
+
+
+# def func(a, b, d):
+#     total = 10
+#     num = 5
+#
+#     for i in range(10):
+#         print(i)
+#
+#     return total
+#
+# n = 10
+# a = 4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
